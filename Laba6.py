8ec2b78240a016673da142a5725f8d262c48aaf7
DanielTretyakov <denownload@vk.com>
2023-11-08 10:32:47 +0700

    create class worker

--- Laba6.py
+++ Laba6.py
@@ -1,9 +1,23 @@
 class Worker:
-    def __init__(self):
-        return
+    def __init__(self, name, surname, age):
+        self.__name = name
+        self.__surname = surname
+        self.__age = age
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def surname(self):
+        return self.__surname
+
+    @property
+    def age(self):
+        return self.__age
 
     def __str__(self):
-        return
+        return f"{self.name} {self.surname}, Возраст: {self.age}"
 
 
 class Boss:
